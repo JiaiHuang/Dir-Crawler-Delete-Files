a0c6eee96f632b2c0a6c8a5987dac0c7d2499bf9
dynamixXs <jiaihuang@outlook.com>
2020-02-01 12:57:40 -0500

    Made changes, and fixed small errors

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import os
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import xlrd
+from datetime import datetime
 
 root = tk.Tk()
 root.title("Directory Crawler Deleter")
 the_canvas = tk.Canvas(root, width = 800, height = 500, bg = 'lightsteelblue')
 the_canvas.pack()
-
 
 def getdir():
     global folder_path
@@ -18,22 +19,47 @@ def getdir():
 def getExcel():
     global df
     import_file_path_excel = filedialog.askopenfilename()
-    df = pd.read_csv(import_file_path_excel)
+    df = pd.read_excel(import_file_path_excel)
     the_canvas.create_text(400, 175, text = import_file_path_excel)
 
 def delete():
-    for folderName, subfolders, filesnames in os.walk(folder_path):
+    dfSuccessLog = pd.DataFrame(columns = ["file name", "file path", "clickable url"])
+    df_fail_log = pd.DataFrame(columns = ["file name", "error", "file path", "clickable url"])
+    for folderName, subfolders, filesnames in os.walk(os.path.normpath(folder_path)):
         print("The current folder is %s" %(folderName))
         for index, row in df.iterrows():
-            fileName = row[df.columns.values[0]]
+            fileName = str(row[df.columns.values[0]])
+            keep = str(row[df.columns.values[1]])
             for name in filesnames:
-                if name == fileName:
-                    os.remove("%s/%s" %(folderName, fileName))
-                    print("Removed %s/%s" %(folderName, fileName))
+                if(pd.notna(fileName)):
+                    fileName = (fileName.replace('%20', ' '))
+                full_path = os.path.join(folderName, name)
+                formatName = name.split(".")[0]
+                if str(formatName).strip().lower() == str(fileName).strip().lower() and keep == 'N':
+                    try:
+                        os.remove(full_path)
+                        new_row_success = {"file name": fileName, "file path": full_path, "clickable url": None}
+                        dfSuccessLog = dfSuccessLog.append(new_row_success, ignore_index =True)
+                        if df["Keep?"][index] == 'N':
+                            index += 1
+                            while pd.isna(df["Keep?"][index]) and pd.isna(df["File Title"][index]):
+                                index+=1
+                                new_row_url = {"file name": None, "file path": None, "clickable url": df["CLICKABLE URL"][index]}
+                                dfSuccessLog = dfSuccessLog.append(new_row_url, ignore_index =True)
+                        print("Removed %s" %(full_path))
+                    except:
+                        print("Error while deleting file, %s" %(full_path))
+                        new_row_fail = {"file name": fileName, "error": "Error while deleing file, %s" %(full_path), "file path": full_path, "clickable url": None}
+                        df_fail_log = df_fail_log.append(new_row_fail, ignore_index =True)
+    print("Finished, please close the application")
+    if(not df_fail_log.empty):
+        df_fail_log.to_excel(datetime.now().strftime("%y-%m-%d_%H-%M-%S") + "_" + "deleteFailLog.xlsx")
+
+    dfSuccessLog.to_excel(datetime.now().strftime("%y-%m-%d_%H-%M-%S") + "_" + "deleteSuccussLog.xlsx")
 
 
 browseButton_dir = tk.Button(text="Import Directory", command = getdir)
-browseButton_excel = tk.Button(text="Import Excel File", command = getExcel)
+browseButton_excel = tk.Button(text="Import excel File", command = getExcel)
 delete_btn = tk.Button(text="Delete Files", command = delete)
 
 the_canvas.create_window(400, 150, window=browseButton_excel)
